2ef36fe2cf6dafff73c4d41e25baa1fb2e7ab513
yunus <yunusileri.tr@gmail.com>
2019-03-16 11:14:31 +0300

    urls

--- BitirmeProjem/settings.py
+++ BitirmeProjem/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
+    
+    
+    'Kullanici.apps.KullaniciConfig'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# BootStrap and JS Directory
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
--- Kullanici/views.py
+++ Kullanici/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def kullanici_home_view(request):
+    return render(request, 'home.html')
